20f6090bf437328803a03bd7ec6cee6d35fdcb2e
John Kelleher <johnpkelleher@gmail.com>
2019-12-19 16:32:51 -0500

    button component

--- src/components/Temp/SwapForm.tsx
+++ src/components/Temp/SwapForm.tsx
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 import BuyToken from './BuyToken'
 import SellToken from './SellToken'
 import Swap from './Swap'
+import Button from './Button'
 
 
-const Container = styled.div`
+const RowContainer = styled.div`
 	font-family: var(--roboto);
 	display: flex;
 	flex-direction: row;
@@ -13,16 +14,29 @@ const Container = styled.div`
 	justify-content: center;
 `
 
+const ColumnContainer = styled.div`
+	font-family: var(--roboto);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 230px;
+`
+
 const SwapForm = ({tokenIn, tokenOut}) => {
 
 	// TODO pass tokenIn and tokenOut
-
 	return(
-		<Container>
-			<BuyToken tokenName="ETH" tokenBalance="2354.52313" tokenAddress="0x009e864923b49263c7F10D19B7f8Ab7a9A5AAd33" />
-			<Swap />
-			<SellToken tokenName="MKR" tokenBalance="1223.12" tokenAddress="0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2" />
-		</Container>
+		<div>
+			<RowContainer>
+				<BuyToken tokenName="ETH" tokenBalance="2354.52313" tokenAddress="0x009e864923b49263c7F10D19B7f8Ab7a9A5AAd33" />
+				<Swap />
+				<SellToken tokenName="MKR" tokenBalance="1223.12" tokenAddress="0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2" />
+			</RowContainer>
+			<ColumnContainer>
+				<Button buttonText="Swap" active={true} />
+			</ColumnContainer>
+		</div>
 	)
 }
 
