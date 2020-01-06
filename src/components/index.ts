31a759b2ab17107e2ab2b1270498140819d1b270
John Kelleher <johnpkelleher@gmail.com>
2020-01-06 16:48:38 -0500

    remove some obsolete files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,7 @@
 import AppBar from './AppBar';
-import Loading from './Loading';
-import LoadingCard from './LoadingCard';
 import SwapForm from './SwapForm';
 import Web3ReactManager from './Web3ReactManager';
 import Header from './Header';
 import TestPanel from './TestPanel';
 
-export { AppBar, Loading, Header, LoadingCard, SwapForm, Web3ReactManager, TestPanel };
+export { AppBar, Header, SwapForm, Web3ReactManager, TestPanel };
