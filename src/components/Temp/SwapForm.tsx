2ddf08c945181d4585314596ea67a6793b5d89cd
tspoff@gmail.com <tspoff@gmail.com>
2020-01-13 22:48:29 -0500

    Add unlock sell assset flow

--- src/components/Temp/SwapForm.tsx
+++ src/components/Temp/SwapForm.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import styled from 'styled-components'
-import BuyToken from './BuyToken'
-import SellToken from './SellToken'
-import Swap from './Swap'
-import Button from './Button'
-import SlippageSelector from './SlippageSelector'
-import TradeComposition from './TradeComposition'
-import AssetSelector from './AssetSelector'
+import React from 'react';
+import styled from 'styled-components';
+import BuyToken from './BuyToken';
+import SellToken from './SellToken';
+import Swap from './Swap';
+import Button from './Button';
+import SlippageSelector from './SlippageSelector';
+import TradeComposition from './TradeComposition';
+import AssetSelector from './AssetSelector';
 
 import { observer } from 'mobx-react';
 import * as helpers from 'utils/helpers';
@@ -14,158 +14,257 @@ import { formNames, labels, SwapMethods } from 'stores/SwapForm';
 import { validators } from '../validators';
 import { useStores } from '../../contexts/storesContext';
 import { ErrorCodes, ErrorIds } from '../../stores/Error';
-import { bigNumberify } from 'ethers/utils';
-import { ContractMetadata } from "../../stores/Token";
-import { checkIsPropertyEmpty, fromWei, toWei } from "utils/helpers";
+import { ContractMetadata } from '../../stores/Token';
+import { checkIsPropertyEmpty, fromWei, toWei } from 'utils/helpers';
 
 const RowContainer = styled.div`
-	font-family: var(--roboto);
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: center;
-`
+    font-family: var(--roboto);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
 
 const ColumnContainer = styled.div`
-	font-family: var(--roboto);
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-`
+    font-family: var(--roboto);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
 
-const SwapForm = observer( ({tokenIn, tokenOut}) => {
+enum ButtonState {
+    NO_WALLET,
+    UNLOCK,
+    SWAP,
+}
 
-	const [modelOpen, setModalOpen] = React.useState({ state: false, input: "inputAmount"})
-	const [tradeCompositionOpen, setTradeCompositionOpen] = React.useState(false)
-	const [slippageSelectorOpen, setSlippageSelectorOpen] = React.useState(false)
+const ButtonText = ['Connect to a wallet', 'Unlock', 'Swap'];
 
-  const {
-      root: {
-          proxyStore,
-          swapFormStore,
-          providerStore,
-          tokenStore,
-          errorStore,
-      },
-  } = useStores();
-
-  const { chainId, account } = providerStore.getActiveWeb3React();
-  const proxyAddress = tokenStore.getProxyAddress(chainId);
-
-  if (!chainId) {
-  		// Review error message
-      throw new Error('ChainId not loaded in TestPanel');
-  }
-
-  const { inputs, outputs } = swapFormStore;
-  const { inputToken, inputTicker, inputIconAddress, outputToken, outputTicker, outputIconAddress } = inputs;
-  const tokenList = tokenStore.getWhitelistedTokenMetadata(chainId);
-
-  // TODO set default inputToken and outputToken to ETH and DAI (or was it token with highest user balance??)
-  if (helpers.checkIsPropertyEmpty(swapFormStore.inputs.inputToken)) {
-      swapFormStore.inputs.inputToken = tokenList[0].address;
-      swapFormStore.inputs.inputTicker = tokenList[0].symbol;
-      swapFormStore.inputs.inputIconAddress = tokenList[0].iconAddress;
-  }
-
-  if (helpers.checkIsPropertyEmpty(swapFormStore.inputs.outputToken)) {
-      swapFormStore.inputs.outputToken = tokenList[1].address;
-      swapFormStore.inputs.outputTicker = tokenList[1].symbol;
-      swapFormStore.inputs.outputIconAddress = tokenList[1].iconAddress;
-  }
-
-  console.log("testing testing testing")
-
-  const swapHandler = async () => {
-      if (!outputs.validSwap) {
-          console.log("swap not valid!" + swapFormStore.outputs.validSwap)
-          console.log(inputs.inputAmount)
-          console.log(inputs.outputAmount)
-          console.log(inputs.type)
-          console.log(inputs.swaps)
-          return;
-      }
-      console.log("swap handler executed");
-
-      if (inputs.type === SwapMethods.EXACT_IN) {
-          const {
-              inputAmount,
-              inputToken,
-              outputToken,
-              outputLimit,
-              limitPrice,
-          } = inputs;
-          await proxyStore.batchSwapExactIn(
-              inputToken,
-              toWei(inputAmount),
-              outputToken,
-              toWei(outputLimit),
-              toWei(limitPrice)
-          );
-      } else if (inputs.type === SwapMethods.EXACT_OUT) {
-          const {
-              inputLimit,
-              inputToken,
-              outputToken,
-              outputAmount,
-              limitPrice,
-          } = inputs;
-          await proxyStore.batchSwapExactOut(
-              inputToken,
-              toWei(inputLimit),
-              outputToken,
-              toWei(outputAmount),
-              toWei(limitPrice)
-          );
-      }
-  };
-
-  const buttonText = account ? 'Swap' : 'Connect to a wallet';
-
-  let inputUserBalanceBN;
-  let inputUserBalance;
-  let outputUserBalanceBN;
-  let outputUserBalance;
-  let userAllowance;
-
-  if (account) {
-    inputUserBalanceBN = tokenStore.getBalance(chainId, inputToken, account)
-    inputUserBalance  = inputUserBalanceBN ? helpers.fromWei(inputUserBalanceBN).toString() : "N/A"
-
-    outputUserBalanceBN = tokenStore.getBalance(chainId, outputToken, account)
-    outputUserBalance  = outputUserBalanceBN ? helpers.fromWei(outputUserBalanceBN).toString() : "N/A"
-
-    userAllowance = tokenStore.getAllowance(
-        chainId,
-        inputToken,
-        account,
-        proxyAddress
+const SwapForm = observer(({ tokenIn, tokenOut }) => {
+    const [modelOpen, setModalOpen] = React.useState({
+        state: false,
+        input: 'inputAmount',
+    });
+    const [tradeCompositionOpen, setTradeCompositionOpen] = React.useState(
+        false
     );
-  }
+    const [slippageSelectorOpen, setSlippageSelectorOpen] = React.useState(
+        false
+    );
 
-  const error = errorStore.getActiveError(ErrorIds.SWAP_FORM_STORE);
-  let errorMessage;
+    const {
+        root: {
+            proxyStore,
+            swapFormStore,
+            providerStore,
+            tokenStore,
+            errorStore,
+        },
+    } = useStores();
 
-  if (error) {
-      console.log('error', error);
-  }
+    const { chainId, account } = providerStore.getActiveWeb3React();
+    const proxyAddress = tokenStore.getProxyAddress(chainId);
 
-	return(
-		<div>
-			<AssetSelector modelOpen={modelOpen} setModalOpen={setModalOpen} />
-			<RowContainer>
-				<BuyToken key="123" inputID="amount-in" inputName="inputAmount" setModalOpen={setModalOpen} tokenName={inputTicker} tokenBalance={inputUserBalance} tokenAddress={inputIconAddress} />
-				<Swap />
-				<SellToken key="122" inputID="amount-out" inputName="outputAmount" setModalOpen={setModalOpen} tokenName={outputTicker} tokenBalance={outputUserBalance} tokenAddress={outputIconAddress} />
-			</RowContainer>
-			<ColumnContainer>
-				<TradeComposition tradeCompositionOpen={tradeCompositionOpen} setTradeCompositionOpen={setTradeCompositionOpen} />
-				<SlippageSelector expectedSlippage="0.38%" slippageSelectorOpen={slippageSelectorOpen} setSlippageSelectorOpen={setSlippageSelectorOpen} />
-				<Button buttonText={buttonText} active={true} onClick={() => {swapHandler()}} />
-			</ColumnContainer>
-		</div>
-	)
-})
+    if (!chainId) {
+        // Review error message
+        throw new Error('ChainId not loaded in TestPanel');
+    }
 
-export default SwapForm
+    const { inputs, outputs } = swapFormStore;
+    const {
+        inputToken,
+        inputTicker,
+        inputIconAddress,
+        outputToken,
+        outputTicker,
+        outputIconAddress,
+    } = inputs;
+    const tokenList = tokenStore.getWhitelistedTokenMetadata(chainId);
+
+    // TODO set default inputToken and outputToken to ETH and DAI (or was it token with highest user balance??)
+    if (helpers.checkIsPropertyEmpty(swapFormStore.inputs.inputToken)) {
+        swapFormStore.inputs.inputToken = tokenList[0].address;
+        swapFormStore.inputs.inputTicker = tokenList[0].symbol;
+        swapFormStore.inputs.inputIconAddress = tokenList[0].iconAddress;
+    }
+
+    if (helpers.checkIsPropertyEmpty(swapFormStore.inputs.outputToken)) {
+        swapFormStore.inputs.outputToken = tokenList[1].address;
+        swapFormStore.inputs.outputTicker = tokenList[1].symbol;
+        swapFormStore.inputs.outputIconAddress = tokenList[1].iconAddress;
+    }
+
+    const buttonActionHandler = (buttonState: ButtonState) => {
+      switch (buttonState) {
+        case ButtonState.NO_WALLET:
+          break;
+        case ButtonState.SWAP:
+          swapHandler();
+          break;
+        case ButtonState.UNLOCK:
+          unlockHandler();
+          break;
+        default:
+          throw new Error('Invalid button state');
+      }
+    };
+
+    const unlockHandler = async () => {
+      const tokenToUnlock = inputs.inputToken;
+      await tokenStore.approveMax(tokenToUnlock, proxyAddress);
+    };
+
+    const swapHandler = async () => {
+        if (!outputs.validSwap) {
+            console.log('swap not valid!' + swapFormStore.outputs.validSwap);
+            console.log(inputs.inputAmount);
+            console.log(inputs.outputAmount);
+            console.log(inputs.type);
+            console.log(inputs.swaps);
+            return;
+        }
+        console.log('swap handler executed');
+
+        if (inputs.type === SwapMethods.EXACT_IN) {
+            const {
+                inputAmount,
+                inputToken,
+                outputToken,
+                outputLimit,
+                limitPrice,
+            } = inputs;
+            await proxyStore.batchSwapExactIn(
+                inputToken,
+                toWei(inputAmount),
+                outputToken,
+                toWei(outputLimit),
+                toWei(limitPrice)
+            );
+        } else if (inputs.type === SwapMethods.EXACT_OUT) {
+            const {
+                inputLimit,
+                inputToken,
+                outputToken,
+                outputAmount,
+                limitPrice,
+            } = inputs;
+            await proxyStore.batchSwapExactOut(
+                inputToken,
+                toWei(inputLimit),
+                outputToken,
+                toWei(outputAmount),
+                toWei(limitPrice)
+            );
+        }
+    };
+
+    const getButtonState = (account, approval): ButtonState => {
+        if (account) {
+            if (!approval) {
+                return ButtonState.UNLOCK;
+            }
+            return ButtonState.SWAP;
+        }
+        return ButtonState.NO_WALLET;
+    };
+
+    const getButtonText = (buttonState: ButtonState): string => {
+      return ButtonText[buttonState];
+    };
+
+    let inputUserBalanceBN;
+    let inputUserBalance;
+    let outputUserBalanceBN;
+    let outputUserBalance;
+    let userAllowance;
+
+    if (account) {
+        inputUserBalanceBN = tokenStore.getBalance(
+            chainId,
+            inputToken,
+            account
+        );
+        inputUserBalance = inputUserBalanceBN
+            ? helpers.fromWei(inputUserBalanceBN).toString()
+            : 'N/A';
+
+        outputUserBalanceBN = tokenStore.getBalance(
+            chainId,
+            outputToken,
+            account
+        );
+        outputUserBalance = outputUserBalanceBN
+            ? helpers.fromWei(outputUserBalanceBN).toString()
+            : 'N/A';
+
+        userAllowance = tokenStore.getAllowance(
+            chainId,
+            inputToken,
+            account,
+            proxyAddress
+        );
+    }
+
+    const buttonState = getButtonState(account, userAllowance && userAllowance.gt(inputs.inputAmount) && userAllowance.gt(0));
+    if (userAllowance) {
+    console.log('User Allowance', {
+        check: userAllowance && userAllowance.gt(inputs.inputAmount) && userAllowance.gt(0),
+        allowance: fromWei(userAllowance)
+    });
+    }
+
+    const error = errorStore.getActiveError(ErrorIds.SWAP_FORM_STORE);
+    let errorMessage;
+
+    if (error) {
+        console.log('error', error);
+    }
+
+    return (
+        <div>
+            <AssetSelector modelOpen={modelOpen} setModalOpen={setModalOpen} />
+            <RowContainer>
+                <BuyToken
+                    key="123"
+                    inputID="amount-in"
+                    inputName="inputAmount"
+                    setModalOpen={setModalOpen}
+                    tokenName={inputTicker}
+                    tokenBalance={inputUserBalance}
+                    tokenAddress={inputIconAddress}
+                />
+                <Swap />
+                <SellToken
+                    key="122"
+                    inputID="amount-out"
+                    inputName="outputAmount"
+                    setModalOpen={setModalOpen}
+                    tokenName={outputTicker}
+                    tokenBalance={outputUserBalance}
+                    tokenAddress={outputIconAddress}
+                />
+            </RowContainer>
+            <ColumnContainer>
+                <TradeComposition
+                    tradeCompositionOpen={tradeCompositionOpen}
+                    setTradeCompositionOpen={setTradeCompositionOpen}
+                />
+                <SlippageSelector
+                    expectedSlippage="0.38%"
+                    slippageSelectorOpen={slippageSelectorOpen}
+                    setSlippageSelectorOpen={setSlippageSelectorOpen}
+                />
+                <Button
+                    buttonText={getButtonText(buttonState)}
+                    active={true}
+                    onClick={() => {
+                      buttonActionHandler(buttonState);
+                    }}
+                />
+            </ColumnContainer>
+        </div>
+    );
+});
+
+export default SwapForm;
