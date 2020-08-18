5498a593a613770956e268170ea49bc5c18206b2
JG <johngrantuk@googlemail.com>
2020-08-18 13:40:10 +0100

    Updated WalletConnect, added Portis and updated connectors.

--- src/provider/connectors.ts
+++ src/provider/connectors.ts
@@ -1,11 +1,18 @@
 import Web3Modal from 'web3modal';
 import WalletConnectProvider from '@walletconnect/web3-provider';
+import Portis from '@portis/web3';
 
 const providerOptions = {
     walletconnect: {
         package: WalletConnectProvider,
         options: {
             infuraId: process.env.REACT_APP_INFURA_ID,
+        },
+    },
+    portis: {
+        package: Portis,
+        options: {
+            id: '3f1c3cfc-7dd5-4e8a-aa03-71ff7396d9fe',
         },
     },
 };
@@ -43,10 +50,18 @@ export const isChainIdSupported = (chainId: number): boolean => {
     return supportedChainId === chainId;
 };
 
+/*
 const RPC_URLS: { [chainId: number]: string } = {
     1: process.env.REACT_APP_RPC_URL_1 as string,
     3: process.env.REACT_APP_RPC_URL_3 as string,
     42: process.env.REACT_APP_RPC_URL_42 as string,
+};
+*/
+
+const WS_URLS: { [chainId: number]: string } = {
+    1: process.env.REACT_APP_WS_URL_1 as string,
+    3: process.env.REACT_APP_WS_URL_3 as string,
+    42: process.env.REACT_APP_WS_URL_42 as string,
 };
 
 export const SUBGRAPH_URLS: { [chainId: number]: string } = {
@@ -56,7 +71,8 @@ export const SUBGRAPH_URLS: { [chainId: number]: string } = {
 };
 
 export const backupUrls = {};
-backupUrls[supportedChainId] = RPC_URLS[supportedChainId];
+// backupUrls[supportedChainId] = RPC_URLS[supportedChainId];
+backupUrls[supportedChainId] = WS_URLS[supportedChainId];
 
 export const SUPPORTED_WALLETS = {
     INJECTED: {
