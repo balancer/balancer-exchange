d4c98ff6c27c0d408a273956d1810427f70e9e93
JG <johngrantuk@googlemail.com>
2020-09-01 09:10:21 +0100

    Check for correct address in asset search.

--- src/stores/Token.ts
+++ src/stores/Token.ts
@@ -5,7 +5,7 @@ import * as helpers from 'utils/helpers';
 import { bnum, formatBalanceTruncated } from 'utils/helpers';
 import { FetchCode } from './Transaction';
 import { BigNumber } from 'utils/bignumber';
-import { MAX_UINT } from 'utils/helpers';
+import { MAX_UINT, isAddress } from 'utils/helpers';
 import { Interface } from 'ethers/utils';
 import * as ethers from 'ethers';
 
@@ -491,6 +491,15 @@ export default class TokenStore {
         account: string
     ): Promise<TokenMetadata> => {
         console.log(`[Token] fetchOnChainTokenMetadata: ${address} ${account}`);
+
+        // This is required as Asset selector search can pass in non-address
+        const checkSumAddr = isAddress(address);
+        if (!checkSumAddr) {
+            console.log(
+                `[Token] fetchOnChainTokenMetadata, not an address: ${address}`
+            );
+            throw new Error(`Incorrect Address Format.`);
+        }
 
         const { providerStore, contractMetadataStore } = this.rootStore;
 
